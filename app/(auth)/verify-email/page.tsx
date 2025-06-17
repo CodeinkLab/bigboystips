df9412fc5039691719dc58959e91115fe8dbc843
@@123@Beatbacklist <dev1samtechstudiolab@gmail.com>
2025-06-17 22:59:40 +0000

    Suspense update

--- app/(auth)/verify-email/page.tsx
+++ app/(auth)/verify-email/page.tsx
@@ -1,5 +1,6 @@
 import { EmailVerificationForm } from '@/app/components/auth/EmailVerificationForm'
 import { Metadata } from 'next'
+import { Suspense } from 'react'
 
 export const metadata: Metadata = {
   title: 'Verify Email | BigBoysTips',
@@ -7,5 +8,9 @@ export const metadata: Metadata = {
 }
 
 export default function VerifyEmailPage() {
-  return <EmailVerificationForm/>
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <EmailVerificationForm />
+    </Suspense>
+  )
 }
