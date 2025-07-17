10c1ec5154f5e2f2cff423afa4e09a7604a9c445
@@123@Beatbacklist <dev1samtechstudiolab@gmail.com>
2025-07-17 14:26:43 +0000

    suspense

--- app/(auth)/reset-password/reset-password-page.tsx
+++ app/(auth)/reset-password/reset-password-page.tsx
@@ -3,10 +3,15 @@
 import { ResetPasswordForm } from '@/app/components/auth/ResetPasswordForm'
 import { ResetPasswordWithToken } from '@/app/components/auth/ResetPasswordWithToken'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
 export default function ResetPasswordPage() {
   const searchParams = useSearchParams()
   const token = searchParams.get('token')
 
-  return token ? <ResetPasswordWithToken /> : <ResetPasswordForm />
+return (
+    <Suspense fallback={<div>Loading...</div>}>
+        {token ? <ResetPasswordWithToken /> : <ResetPasswordForm />}
+    </Suspense>
+)
 }
